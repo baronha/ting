ba2baaaafcf7710bf78eea9dc26e19066edccb89
Bảo Hà <baronn.ha@gmail.com>
2023-10-09 15:37:07 +0700

    refactor: remove expo description

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,7 @@ export * from './Type';
 const LINKING_ERROR =
   `The package 'ting' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  '- You rebuilt the app after installing the package\n'
 
 // @ts-expect-error
 const isTurboModuleEnabled = global.__turboModuleProxy != null;
